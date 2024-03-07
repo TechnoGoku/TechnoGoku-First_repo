96eb24ed48f141e1a3bf4e32dd671efa9668c675
TechnoGoku <bulatikilya@gmail.com>
2024-03-08 00:42:15 +0200

    update_4

--- hw_task1/hw1.py
+++ hw_task1/hw1.py
@@ -8,8 +8,9 @@ today_time = datetime.today()
 def get_days_from_today(date) -> int:
     your_date_object = datetime.strptime(your_date, "%Y-%m-%d")
     difference = today_time.toordinal() - your_date_object.toordinal()
-    print(your_date_object)
-    print(difference)
+    print(f"Ваша дата у форматі РРРР-ММ-ДД об'єкту datetime: {your_date_object}")
+    print(f"Різниця дорівнює: {difference}")
+    return difference
    
 
 your_date = input("Введіть вашу дату у форматі РРРР-ММ-ДД: ")
