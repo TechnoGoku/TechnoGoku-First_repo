b96cbb148789845db4a383827f2e6b5c83d2e7d9
TechnoGoku <bulatikilya@gmail.com>
2024-03-08 00:33:04 +0200

    update_3

--- hw_task1/hw1.py
+++ hw_task1/hw1.py
@@ -7,9 +7,11 @@ today_time = datetime.today()
 
 def get_days_from_today(date) -> int:
     your_date_object = datetime.strptime(your_date, "%Y-%m-%d")
-    print(f"Ваша дата у форматі РРРР-ММ-ДД: ")
+    difference = today_time.toordinal() - your_date_object.toordinal()
     print(your_date_object)
+    print(difference)
+   
 
 your_date = input("Введіть вашу дату у форматі РРРР-ММ-ДД: ")
 get_days_from_today(your_date)
-print(your_date)
+print(f"Ваша дата у форматі РРРР-ММ-ДД: {your_date} ")
