579462c66fa58e124ea3ae13de88b2110ce657e5
TechnoGoku <bulatikilya@gmail.com>
2024-03-08 00:21:19 +0200

    update_2

--- hw_task1/hw1.py
+++ hw_task1/hw1.py
@@ -3,6 +3,13 @@ from datetime import datetime
 date = "2021-10-09"
 
 today_time = datetime.today()
-print(today_time)
 
-def get_days_from_today(date = "2021-10-09"):
+
+def get_days_from_today(date) -> int:
+    your_date_object = datetime.strptime(your_date, "%Y-%m-%d")
+    print(f"Ваша дата у форматі РРРР-ММ-ДД: ")
+    print(your_date_object)
+
+your_date = input("Введіть вашу дату у форматі РРРР-ММ-ДД: ")
+get_days_from_today(your_date)
+print(your_date)
