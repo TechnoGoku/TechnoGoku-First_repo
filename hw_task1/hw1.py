8d8338bc47146c6f91532a6e638bd03c82b0366b
TechnoGoku <bulatikilya@gmail.com>
2024-03-08 00:58:33 +0200

    update_4

--- hw_task1/hw1.py
+++ hw_task1/hw1.py
@@ -6,13 +6,19 @@ today_time = datetime.today()
 
 
 def get_days_from_today(date) -> int:
-    your_date_object = datetime.strptime(your_date, "%Y-%m-%d")
-    difference = today_time.toordinal() - your_date_object.toordinal()
-    print(f"Ваша дата у форматі РРРР-ММ-ДД об'єкту datetime: {your_date_object}")
-    print(f"Різниця дорівнює: {difference}")
-    return difference
-   
+    try:
+        your_date_object = datetime.strptime(your_date, "%Y-%m-%d")
+        difference = today_time.toordinal() - your_date_object.toordinal()
+        print(f"Ваша дата у форматі РРРР-ММ-ДД об'єкту datetime: {your_date_object}")
+        print(f"Різниця дорівнює: {difference} днів")
+        print(f"Ваша дата у форматі РРРР-ММ-ДД: {your_date} ")
+        return difference
+    except ValueError:
+        print("Ви ввели дату невірного формату, потрібно ввести дату формату РРРР-ММ-ДД с дефісом '-'!")
+
+
+    
 
 your_date = input("Введіть вашу дату у форматі РРРР-ММ-ДД: ")
 get_days_from_today(your_date)
-print(f"Ваша дата у форматі РРРР-ММ-ДД: {your_date} ")
+
